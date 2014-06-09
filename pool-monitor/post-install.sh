fa32d1ca827ab83c45197a0dda2b76c33a97de07
Dewitt Buckingham <dj729@comcast.net>
2014-06-09 13:56:28 -0700

    add install script

---
+++ pool-monitor/post-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Init
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SIREN=siren
+CONFIG=siren.cfg
+
+if [[ EUID -ne 0 ]]; then
+    echo "ROOT!!!"
+    exit 1
+else
+    ln -s $DIR/$SIREN /bin/$SIREN
+    ln -s $DIR/$CONFIG /etc/$CONFIG
+fi
+
